4fb2e0aa28890df602712dd70793eabd1e99a5e5
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2024-10-12 16:12:30 +0500

    change tasgs model

--- src/apps/tags/models/tags.py
+++ src/apps/tags/models/tags.py
@@ -6,6 +6,8 @@ from apps.base.models import AbstractBaseModel
 class Tag(AbstractBaseModel):
     name = models.SlugField(max_length=25)
 
+    is_active = models.BooleanField(default=False)
+
     class Meta:
         db_table = 'tag'
 
