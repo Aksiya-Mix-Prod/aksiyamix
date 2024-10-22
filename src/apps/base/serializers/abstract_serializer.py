dc1bfc516d9c0e8ceab914ca46604606f895c0a8
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2024-10-22 14:14:55 +0500

    change AbstractBaseModel

---
+++ src/apps/base/serializers/abstract_serializer.py
@@ -0,0 +1,21 @@
+class AbstractCustomSerializerMixin:
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            validated_data['created_by'] = validated_data['updated_by'] = user
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            validated_data['updated_by'] = user
+        return super().update(instance, validated_data)
+
+    def save(self, **kwargs):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            if self.instance is None:
+                kwargs['created_by'] = self.context['request'].user
+            kwargs['updated_by'] = self.context['request'].user
+        return super().save(**kwargs)
