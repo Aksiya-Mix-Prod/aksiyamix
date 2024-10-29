463cb9b0ffe0649be45411b87694ee0fcb9162db
jvokh1r <javokh1r.kobiljonov2007@gmail.com>
2024-10-29 15:58:49 +0500

    categories done

--- src/apps/categories/serializers/category.py
+++ src/apps/categories/serializers/category.py
@@ -1,5 +1,3 @@
-from rest_framework import serializers
-
 from apps.categories.models.category import Category
 from apps.base.serializers import CustomModelSerializer
 
@@ -12,14 +10,4 @@ class CategorySerializer(CustomModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'slug', 'icon', 'parent', 'created_at']
-
-
-    def get_children(self, slug):
-        """ Get category children """
-
-        children = Category.objects.filter(parent_id=slug)
-        if children.exists():
-            return CategorySerializer(children, many=True).data
-        return None
-
 
--- src/apps/categories/views/category.py
+++ src/apps/categories/views/category.py
@@ -1,20 +1,19 @@
-from jedi.inference.gradual.typeshed import try_to_load_stub_cached
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from yaml import serialize
-
-from apps.base.exceptions import CustomExceptionError
 from apps.categories.serializers.category import CategorySerializer
 from apps.categories.models.category import Category
-from apps.base.views import CustomViewSet, CustomModelViewSet
+from apps.base.views import CustomReadOnlyModelViewSet
 
 
-class CategoryListViewSet(CustomViewSet):
+class CategoryListViewSet(CustomReadOnlyModelViewSet):
+    queryset = Category.objects.filter(parent__isnull=True)
+    serializer_class = CategorySerializer
 
-    def list(self, request):
-        categories = Category.objects.filter(parent=None) # get parent(does not have parent) categories
-        serializer = CategorySerializer(categories, many=True)
-        return Response({'categories': serializer.data})
+
+class ChildrenCategoryViewSet(CustomReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        parent_id = self.kwargs['parent_id']
+        return Category.objects.filter(parent_id=parent_id)
 
 
 
